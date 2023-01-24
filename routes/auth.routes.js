f66127ecb6f0cd135ca0ad4cbfe6ba6317e25761
valentine974 <valentine.bathany@hotmail.fr>
2023-01-24 21:08:20 +0100

    added routes, still missing authentification & authorizaiton

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const Users = require('../models/User.model');
 const router = express.Router();
+ 
 
 /* GET home page */
 
@@ -7,8 +9,58 @@ router.get("/login", (req, res, next) => {
     res.render("auth/login");
   });
 
+
+  router.post("/login", (req, res, next) => {
+
+    console.log(req.body)
+
+    const {email, hashedPassword} = req.body
+
+    if(email ==="" || hashedPassword==="" ) {
+      res.render('auth/login',{errorMessage:'fill both email & pwd'})
+      return
+    }
+  
+    Users.findOne({email})
+    .then((user)=>{
+      if(!user){
+        res.render('auth/login',{errorMessage:"user doesn't exist"})
+        return
+      } else if (user.hashedPassword === hashedPassword) {
+        res.redirect(`/profile/${user._id}`)
+      } else {
+        res.render('auth/login',{errorMessage:"incorrect pwd"})
+      }
+ 
+    })
+    .catch(err=>console.log(`error with the login ${err}`))
+    
+  });
+
+
 router.get("/signup", (req, res, next) => {
+
   res.render("auth/signup");
 });
+
+router.post("/signup", (req, res, next) => {
+  
+  Users.create(req.body)
+  .then((result)=>{
+    console.log(result._id)
+    res.redirect(`/profile/${result._id}`);
+  })
+  .catch(err=>console.log(`error with the signup ${err}`))
+  
+});
+
+
+router.post("/logout",(req,res,next)=>{
+  // req.session.destroy(err=>{
+  //   if(err) next (err);
+  //   res.redirect('/')
+  // })
+  res.redirect('/')
+})
 
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const { route } = require('./index.routes');
 const router = express.Router();
+const Users = require('../models/User.model'); 
 
 /* GET home page */
 
-router.get("/profile/id", (req, res, next) => {
-    res.render("user/my-profile");
+router.get("/profile/:id", (req, res, next) => {
+  Users.findById(req.params.id)
+  .then((result)=>{
+    res.render("user/my-profile",result);
+  })
+
+  .catch(err=>console.log(`error with the profile page: ${err}`))
   });
 
-router.get("/id/shelf", (req, res, next) => {
-  res.render("user/my-shelf");
+router.get("/profile/:id/shelf", (req, res, next) => {
+  Users.findById(req.params.id)
+  .then((result)=>{
+    res.render("user/my-shelf",result);
+  }) 
 });
 
 
