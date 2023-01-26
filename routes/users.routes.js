9c4ae7141e0a04403bf4d70ab124799e7ed93641
valentine974 <valentine.bathany@hotmail.fr>
2023-01-26 21:29:51 +0100

    added middleware

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,19 +2,28 @@ const express = require('express');
 const { route } = require('./index.routes');
 const router = express.Router();
 const Users = require('../models/User.model'); 
+const Books = require('../models/Books.model')
+const {isLoggedIn, isLoggedOut} = require('../middlewares/route-guard'); 
 
 /* GET home page */
 
-router.get("/profile", (req, res, next) => {
+router.get("/profile",isLoggedIn, (req, res, next) => {
   res.render("user/my-profile",{userInSession: req.session.currentUser});
   });
 
-router.get("/profile/:id/shelf", (req, res, next) => {
-  Users.findById(req.params.id)
-  .then((result)=>{
-    res.render("user/my-shelf",result);
-  }) 
+router.get("/profile/shelf", isLoggedIn,(req, res, next) => {
+    res.render("user/my-shelf",{userInSession: req.session.currentUser});
+ 
 });
+
+router.post('/add-book/:id',(req,res,next)=>{
+  Users.findByIdAndUpdate(req.session.currentUser._id,{$push:{books:req.params.id}},{new:true})
+  .then(()=>{res.redirect('/profile/shelf')})
+  
+  .catch((err)=>console.log('something went wrong with adding books',err))
+
+
+})
 
 
 module.exports = router;
