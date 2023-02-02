45892a9b05d1316579e6392fbbc1810a941a17e9
valentine974 <valentine.bathany@hotmail.fr>
2023-02-02 21:49:46 +0100

    MVP DONE !!!

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,7 +3,9 @@ const { route } = require("./index.routes");
 const router = express.Router();
 const Users = require("../models/User.model");
 const { isLoggedIn, isLoggedOut } = require("../middlewares/route-guard");
-const axios = require('axios')
+const axios = require('axios');
+const asyncFor = require("../utils/asyncFor");
+const callbackFor = require("../utils/callbackFor");
 
  
 /* GET home page */
@@ -12,22 +14,15 @@ router.get("/profile", isLoggedIn, (req, res, next) => {
   res.render("user/my-profile", { userInSession: req.session.currentUser });
 });
 
-router.get("/profile/shelf", isLoggedIn, (req, res, next) => {
+router.get("/profile/shelf", isLoggedIn, (req, res, next) => { 
+  let books =[]
   Users.findById(req.session.currentUser._id)
     .then((user) => {
       const bookIds = user.books
-      let books 
-      for(let i in bookIds){
-        axios.get(`https://www.googleapis.com/books/v1/volumes/${bookIds[i]}`)
-        .then(result => {
-          console.log(typeof result.data)
-          books.push(result.data)
-        })
-        .catch(err => console.log("book not pushed in array of books", err))
-      }
-      return books
+      return asyncFor(bookIds,callbackFor,books)
+
     })
-    .then(books => {
+    .then(() => {
       console.log(books)
       res.render("user/my-shelf",{books})})
     .catch((err) =>console.log("something went wrong with showing shelf", err));
