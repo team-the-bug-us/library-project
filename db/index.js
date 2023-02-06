c286d15d1e543c5c6d8a0f754b480f9fe70b6c24
sw-li <lishengwei90@gmail.com>
2023-02-06 17:34:29 +0100

    hide mongodb link from users

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 const Book = require("../models/Books.model")
-
+/* 
 const books = [
   {
     title: "Le deuxième sexe",
@@ -42,13 +42,12 @@ const books = [
     price : 30,
   }
 ]
-
+ */
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb+srv://Shengwei:Azerty1@bug-us.b4jh0zn.mongodb.net/bug-us?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGODB_URI;
 
 mongoose
   .connect(MONGO_URI)
