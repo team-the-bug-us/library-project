c35c7a0f0643f1b82e3f9ce969cd43e4638e2403
valentine974 <valentine.bathany@hotmail.fr>
2023-02-07 19:38:49 +0100

    refreshing main branch

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-
-
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("library-project JS imported successfully!");
-});
-
--- utils/asyncFor.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (array, callback, arrayToPush) => {
-    for (let i = 0; i < array.length; i += 1) {
-      const result = await callback(array[i])
-  
-      arrayToPush.push(result)
-    }
-  };
