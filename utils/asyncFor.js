74d044fc4194a9d8bfddfb693b35ff944083830e
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-02-07 20:33:39 +0100

    default images moved to cloudinary

--- utils/asyncFor.js
+++ utils/asyncFor.js
@@ -1,6 +1,24 @@
 module.exports = async (array, callback, arrayToPush) => {
     for (let i = 0; i < array.length; i += 1) {
-      const result = await callback(array[i])
-      arrayToPush.push(result)
+      const data = await callback(array[i])
+    const fillingImgUrl =
+  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/mystery-book-cover-design-template-a5dce61a0c99630dedab42e3a4c15618_screen.jpg?ts=1637014687";
+
+    const imageLinks = data.volumeInfo.imageLinks;
+    if (imageLinks) {
+      data.volumeInfo.imageLinks =
+        imageLinks.extraLarge ||
+        imageLinks.large ||
+        imageLinks.medium ||
+        imageLinks.small ||
+        imageLinks.thumbnail ||
+        imageLinks.smallThumbnail;
+    } else {
+      data.volumeInfo.imageLinks = fillingImgUrl;
+    }
+    console.log(data.volumeInfo.imageLinks)
+      
+
+      arrayToPush.push(data)
     }  
   };
--- utils/callbackFor.js
+++ utils/callbackFor.js
@@ -5,22 +5,22 @@ module.exports = async (bookId) => {
     const { data } = await axios.get(
       `https://www.googleapis.com/books/v1/volumes/${bookId}`
     );
-//     const fillingImgUrl =
-//   "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/mystery-book-cover-design-template-a5dce61a0c99630dedab42e3a4c15618_screen.jpg?ts=1637014687";
+    const fillingImgUrl =
+  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/mystery-book-cover-design-template-a5dce61a0c99630dedab42e3a4c15618_screen.jpg?ts=1637014687";
 
-//     const imageLinks = data.volumeInfo.imageLinks;
-//     if (imageLinks) {
-//       data.volumeInfo.imageLinks =
-//         imageLinks.extraLarge ||
-//         imageLinks.large ||
-//         imageLinks.medium ||
-//         imageLinks.small ||
-//         imageLinks.thumbnail ||
-//         imageLinks.smallThumbnail;
-//     } else {
-//       data.volumeInfo.imageLinks = fillingImgUrl;
-//     }
-    // console.log(data.volumeInfo.imageLinks)
+    const imageLinks = data.volumeInfo.imageLinks;
+    if (imageLinks) {
+      data.volumeInfo.imageLinks =
+        imageLinks.extraLarge ||
+        imageLinks.large ||
+        imageLinks.medium ||
+        imageLinks.small ||
+        imageLinks.thumbnail ||
+        imageLinks.smallThumbnail;
+    } else {
+      data.volumeInfo.imageLinks = fillingImgUrl;
+    }
+    console.log(data.volumeInfo.imageLinks)
     return data;
   } catch (error) {
     console.log("the issue with async:", error);
